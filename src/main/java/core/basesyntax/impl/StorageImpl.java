9fcb02e924d1c8f9e60a56ada99d61d566464a71
user <pawel.m.janik@gmail.com>
2026-07-07 16:48:50 +0200

    Task complete. Implemented logic for StorageImpl. Logic receives and stores generic data types. If key already exists in the array it is updated. Method size() implemented to return current number of key value pairs stored. Method get() implemented to retrieve value using key. Method put() implemented to to update or add new key value pairs. Class Entry created to store input data.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,48 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+    @SuppressWarnings("unchecked")
+    private final Entry<K, V>[] storage = (Entry<K, V>[]) new Entry[CAPACITY];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(storage[i].key, key)) {
+                storage[i].value = value;
+                return;
+            }
+        }
+        storage[size] = new Entry<>(key, value);
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(storage[i].key, key)) {
+                return storage[i].value;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
